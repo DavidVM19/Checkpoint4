b942b0bda9b8d69807acead25e44f53704003ea3
DavidVM19 <davidvm1910@gmail.com>
2022-02-02 16:41:35 +0100

    create routes and text in Home

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,5 +1,5 @@
 import Poster from "../../../resources/homePGWM.png";
-import Header from "../layout/Header";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -8,6 +8,29 @@ const Home = () => {
         <img src={Poster} alt="PGWM" />
       </div>
       <h1>Bienvenue sur PGWM / Play Games Win Money !</h1>
+      <div className="home__body">
+        <p>
+          Tous les joueurs veulent tester leur talent. PGWM est{" "}
+          <strong>LE</strong> site sur lequel vous pouvez parier sur vous-même
+          dans des compétitions de jeux vidéo. Vous pouvez jouer contre des
+          joueurs du monde entier, en quelques clics et prouver votre maîtrise
+          des plus grands jeux videos.{" "}
+        </p>
+        <br />
+        <p>
+          FIFA, NBA 2K, Call Of Duty, le but est simple :{" "}
+          <strong>LA VICTOIRE OU RIEN</strong>
+        </p>
+        <p>
+          Alors n'attendez plus, inscrivez vous et affrontez les adversaires
+          pour prouver votre valeur !
+        </p>
+        <Link to="/connection">
+          <button type="button" className="btn">
+            Goooo !
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
